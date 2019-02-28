9ec27bf6c57df281db30b4c01f96dc4b371302ed
Muhammed Okumuş <muhammedokumus@Muhammeds-MacBook-Pro.local>
2019-03-01 02:48:06 +0300

    Initial Commit

---
+++ HW1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HW1
+//
+//  Created by Muhammed Okumuş on 1.03.2019.
+//  Copyright © 2019 Muhammed Okumuş. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
