16c2a69e4a636e69fe9558bb6be22f514f223968
Muhammed Okumuş <muhammedokumus@muhammeds-macbook-pro.local>
2019-03-05 12:36:14 +0300

    DFS skeleton

--- HW1/main.c
+++ HW1/main.c
@@ -12,13 +12,20 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <string.h>
+#include <dirent.h>
 
 long int sizepathfun (const char *path);
-int sizepathfun2 (const char *path);
-
+long int depthFirstApply (const char *path, long int pathfun (const char *path1));
+void listdir(const char *name);
 
 int main(int argc, const char * argv[]) {
+   
+   // depthFirstApply(argv[1], sizepathfun);
+    listdir(argv[1]);
 
+    
+    
+    
     //No arguments given
     if(argv[1] == NULL) {
         printf(">No argument run\n");
@@ -50,6 +57,56 @@ int main(int argc, const char * argv[]) {
 }
 
 
+void listdir(const char *name)
+{
+    DIR *d;
+    struct dirent *DIR;
+    
+    if (!(d = opendir(name)))
+        return;
+    if (!(DIR = readdir(d)))
+        return;
+    
+    do {
+        if (DIR->d_type == DT_DIR) {
+            char path[1024];
+            int len = snprintf(path, sizeof(path)-1, "%s/%s", name, DIR->d_name);
+            path[len] = 0;
+            if (strncmp(DIR->d_name, ".",1) == 0)
+                continue;
+            
+            printf("path: %s\n", path);
+           // printf("%s\n", DIR->d_name);
+           
+            listdir(path);
+        }
+        else{
+            printf("%s\n", DIR->d_name);
+        }
+    } while ((DIR = readdir(d)));
+    closedir(d);
+}
+
+
+long int depthFirstApply (const char *path, long int pathfun (const char *path1)){
+    DIR *d;
+    struct dirent * dir;
+  
+    d = opendir(path);
+    while((dir = readdir(d)) != NULL) {
+        char filepath[1024] ;
+        strcpy(filepath, path);
+        strcat(filepath, "/");
+        strcat(filepath, dir->d_name);
+    
+        
+        sizepathfun(filepath);
+    }
+    
+    closedir(d);
+    return pathfun(path);
+}
+
 
 long int sizepathfun (const char *path) {
     struct stat fileStat;
@@ -59,49 +116,12 @@ long int sizepathfun (const char *path) {
         return -1;
     }
     
-    printf("Information for %s\n",path);
-    printf("---------------------------\n");
-    printf("File Size: \t\t%lld bytes\n",fileStat.st_size);
-    printf("Number of Links: \t%d\n",fileStat.st_nlink);
-    printf("File inode: \t\t%llu\n",fileStat.st_ino);
-
-    
-    printf("The file %s a symbolic link\n", (S_ISLNK(fileStat.st_mode)) ? "is" : "is not");
-    printf("off_t: %lu \n", sizeof(off_t));
-    printf("long int: %lu \n", sizeof(long int));
-    return fileStat.st_size;
-}
-
-
-int sizepathfun2 (const char *path) {
-    struct stat fileStat;
-    
-    if(stat(path,&fileStat) < 0){
-        printf("Failed to read path\n");
-        return -1;
+    if(S_ISLNK(fileStat.st_mode)){
+        printf("Special file beware");
+        return fileStat.st_size;
     }
     
-    printf("Information for %s\n",path);
-    printf("---------------------------\n");
-    printf("File Size: \t\t%lld bytes\n",fileStat.st_size);
-    printf("Number of Links: \t%d\n",fileStat.st_nlink);
-    printf("File inode: \t\t%llu\n",fileStat.st_ino);
-    
-    printf("File Permissions: \t");
-    printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
-    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
-    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
-    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
-    printf("\n\n");
-    
-    printf("The file %s a symbolic link\n", (S_ISLNK(fileStat.st_mode)) ? "is" : "is not");
-
+    printf("%-8lld %s\n", fileStat.st_size, path);
    
-    return (int) fileStat.st_size;
+    return fileStat.st_size;
 }
