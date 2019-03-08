8d56fe7b3ebe2feb95dfb8a03c3196bda9eb17c5
Muhammed Okumuş <rocketpolka@gmail.com>
2019-03-08 16:49:21 +0300

    Add files via upload

---
+++ HW1/BuNeDu.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ftw.h>
+#include <dirent.h>
+#include <string.h>
+
+static unsigned int total = 0;
+static unsigned int special = 0;
+static unsigned int zflag = 0; //0 is off, 1 is on
+
+int sum(const char *fpath, const struct stat *sb, int typeflag);
+int wrapper(const char *path, int sum (const char *fpath, const struct stat *sb, int typeflag));
+int sizepathfun (char *path);
+int postOrderApply (char *path, int pathfun (char *path1));
+
+int main(int argc, char **argv) {
+    int result = -1;
+    
+    if(argc == 2){
+        result = postOrderApply(argv[1], sizepathfun);
+        sizepathfun(argv[1]);
+        return result;
+    }
+    
+    else if(argc == 3){
+        //-z flag is not implemented but argument format is supported
+        zflag = 1;
+        result = postOrderApply(argv[2], sizepathfun);
+        sizepathfun(argv[2]);
+        return result;
+    }
+    
+    printf("Usage \n--------- \n");
+    printf("%s path/to/folder\n", argv[0]);
+    printf("%s -z path/to/folder\n", argv[0]);
+    
+    return result;
+}
+
+int sum(const char *fpath, const struct stat *sb, int typeflag) {
+    total += sb->st_size;
+    return 0;
+}
+
+int wrapper(const char *path,int sum (const char *fpath, const struct stat *sb, int typeflag)){
+    if (ftw(path, sum, 1)) {
+        perror("ftw error");
+        return -1;
+    }
+    printf("%s: %u\n", path, total);
+    return 0;
+}
+
+int sizepathfun (char *path){
+    int temp;
+    if (ftw(path, &sum, 1)) {
+        perror("ftw");
+        return -1;
+    }
+    temp = total;
+    total = 0;
+    printf("%-10u %s\n", temp, path);
+    return temp;
+}
+
+int postOrderApply (char *path, int pathfun (char *path1)){
+    DIR *dir;
+    struct dirent *entry;
+    
+    if (!(dir = opendir(path)))
+        return -1;
+    
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            char filepath[1024];
+            //Avoid loops and ignore hidden files like .DS_Store
+            if (strncmp(entry->d_name, ".",1) == 0)
+                continue;
+            
+            //Construct path to child
+            strcpy(filepath, path);
+            strcat(filepath, "/");
+            strcat(filepath, entry->d_name);
+            
+            pathfun(filepath);
+            postOrderApply(filepath, pathfun);
+        }
+        else
+            printf("Special file beware%d\n",special++);
+    }
+    closedir(dir);
+    return -1;
+}
+
---
+++ HW1/readme.txt
@@ -0,0 +1,13 @@
+Yararlanılan kaynaklar:
+1) https://stackoverflow.com/questions/8436841/how-to-recursively-list-directories-in-c-on-linux/29402705
+    -postOrderApply fonksiyonunda kullanılan kod parçaları.
+2) https://stackoverflow.com/questions/472697/how-do-i-get-the-size-of-a-directory-in-c
+    -ftw.h kütüphanesinin anlaşılması için incelendi, wrapper ve sum fonksiyonlarda kod parçaları.
+
+
+
+Makefile:
+
+make
+
+clean
